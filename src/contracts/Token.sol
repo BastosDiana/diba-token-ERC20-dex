b8bac477139f3d6401d63f93e71b150785658d1f
Diana Bastos <bastosdsb@gmail.com>
2024-08-15 17:56:42 +0100

    feat: Update Token contract && enhance tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -6,8 +6,24 @@ contract Token {
   string public symbol = "DiBa";
   uint256 public decimals = 18;
   uint256 public totalSupply;
+  mapping(address => uint256) public balanceOf;
 
-  constructor() public {
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  constructor() {
     totalSupply = 1000000 * (10 ** decimals);
+    balanceOf[msg.sender] = totalSupply;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(_to != address(0), "Invalid address");
+    require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
   }
 }
---
+++ test/helpers.js
@@ -0,0 +1,10 @@
+const EVM_REVERT = "VM Exception while processing transaction: revert Insufficient balance"
+
+const ether = (n) => {
+  return new web3.utils.BN(web3.utils.toWei(n.toString(), "ether"));
+};
+
+module.exports = {
+  ether,
+  EVM_REVERT
+};
