977c77e107d1eadb8c04eecdf6ebfc0bede9e0a5
Diana Bastos <bastosdsb@gmail.com>
2024-08-21 21:22:41 +0100

    feat: Add fillOrder functionality && update tests

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -10,10 +10,10 @@ import "./Token.sol";
 // [X] Deposit tokens
 // [X] Withdraw tokens
 // [X] Check balances
-// [ ] Make order
-// [ ] Cancel order
-// [ ] Fill order
-// [ ] Charge fees
+// [X] Make order
+// [X] Cancel order
+// [X] Fill order
+// [X] Charge fees
 
 contract Exchange {
     address public feeAccount; // the account that receives exchange fees
@@ -23,11 +23,12 @@ contract Exchange {
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
     mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
 
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint amount, uint balance);
-     event Order(
+    event Order(
         uint256 id,
         address user,
         address tokenGet,
@@ -43,6 +44,16 @@ contract Exchange {
         uint256 amountGet,
         address tokenGive,
         uint256 amountGive,
+        uint256 timestamp
+    );
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address userFill,
         uint256 timestamp
     );
 
@@ -78,7 +89,10 @@ contract Exchange {
     }
 
     function withdrawEther(uint _amount) public {
-        require(tokens[ETHER][msg.sender] >= _amount, "Insufficient amount of Ether to withdraw");
+        require(
+            tokens[ETHER][msg.sender] >= _amount,
+            "Insufficient amount of Ether to withdraw"
+        );
         tokens[ETHER][msg.sender] -= _amount;
         // Send Ether - return Ether back to the original user
         payable(msg.sender).transfer(_amount);
@@ -101,21 +115,101 @@ contract Exchange {
         emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
-    function balanceOf(address _token, address _user) public view returns (uint256) {
+    function balanceOf(
+        address _token,
+        address _user
+    ) public view returns (uint256) {
         return tokens[_token][_user];
     }
 
-    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
-        orderCount += orderCount;
-        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
-        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+    function makeOrder(
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) public {
+        orderCount++;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
+        );
     }
 
     function cancelOrder(uint256 _id) public {
         _Order storage _order = orders[_id];
-        require(address(_order.user) == msg.sender, "Unkonwn user, it should be you canceling the order");
+        require(
+            address(_order.user) == msg.sender,
+            "Unkonwn user, it should be you canceling the order"
+        );
         require(_order.id == _id, "Invalid order. The order must exist!"); // The order must exist
         orderCancelled[_id] = true;
-        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
+
+    function fillOrder(uint256 _id) public {
+        require(_id > 0 && _id <= orderCount);
+        require(!orderFilled[_id]);
+        require(!orderCancelled[_id]);
+        _Order storage _order = orders[_id];
+        _trade(
+            _order.id,
+            _order.user,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive
+        );
+        orderFilled[_order.id] = true;
+    }
+
+    function _trade(
+        uint256 _orderId,
+        address _user,
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
+        uint256 _amountGive
+    ) internal {
+        // Fee paid by the user that fills the order, a.k.a. msg.sender.
+        uint256 _feeAmount = (_amountGet * feePercent) / 100;
+
+        tokens[_tokenGet][msg.sender] -= (_amountGet + _feeAmount);
+        tokens[_tokenGet][_user] += _amountGet;
+        tokens[_tokenGet][feeAccount] += _feeAmount;
+        tokens[_tokenGive][_user] -= _amountGive;
+        tokens[_tokenGive][msg.sender] += _amountGive;
+
+        emit Trade(
+            _orderId,
+            _user,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            msg.sender,
+            block.timestamp
+        );
     }
 }
